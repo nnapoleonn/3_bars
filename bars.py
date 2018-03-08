c3273b9ff7577982b1381eed694125ae39125aa0
nnapoleonn <nnapoleonn@mail.ru>
2018-03-08 10:29:14 +0300

    script finished

--- bars.py
+++ bars.py
@@ -2,27 +2,24 @@ import json
 import sys
 import math
 
-if len(sys.argv) == 1:
-    sys.exit('Path to file is not found!')
-
-file_path = sys.argv[1]
-
 
 def load_data(file_path):
     with open(file_path, encoding='utf8') as file_data:
         return json.loads(file_data.read())
 
 
-def get_biggest_bar(data_from_object):
-    return max(data_from_object, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+def get_biggest_bar(bars):
+    return max(bars, key=lambda x:
+    x['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(data_from_object):
-    return min(data_from_object, key=lambda x: x['properties']['Attributes']['SeatsCount'])
+def get_smallest_bar(bars):
+    return min(bars, key=lambda x:
+    x['properties']['Attributes']['SeatsCount'])
 
 
-def get_closest_bar(data_from_object, longitude, latitude):
-    return min(data_from_object, key=lambda x:
+def get_closest_bar(bars, longitude, latitude):
+    return min(bars, key=lambda x:
         math.sqrt((x['geometry']['coordinates'][0] - longitude)**2 +
                  (x['geometry']['coordinates'][1] - latitude)**2))
 
@@ -38,14 +35,30 @@ def give_output(data_about_bars):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.exit('Path to file is not found!')
+    file_path = sys.argv[1]
     try:
         longitude = float(input('Enter your longitude: '))
         latitude = float(input('Enter your latitude: '))
     except ValueError:
         sys.exit('Wrong coordinate!')
+
+    strings_from_json = load_data(file_path)['features']
     print('The BIGGEST bar is:')
-    give_output(get_biggest_bar(load_data(file_path)['features']))
+
+    give_output(
+        get_biggest_bar(
+            strings_from_json))
+
     print('The SMALLEST bar is:')
-    give_output(get_smallest_bar(load_data(file_path)['features']))
+
+    give_output(
+        get_smallest_bar(
+            strings_from_json))
+
     print('The CLOSEST bar is:')
-    give_output(get_closest_bar(load_data(file_path)['features'], longitude, latitude))
+
+    give_output(
+        get_closest_bar(
+            strings_from_json,longitude, latitude))
